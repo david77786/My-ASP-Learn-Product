8f4771a835ad8eb0e28faa3383c49ea35157e10e
david77786 <davidsu77786@gmail.com>
2021-09-27 19:57:03 +0800

    首次上傳
    建立MVC專案
    並以連接DB

---
+++ Leaning Product/Leaning Product/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Leaning_Product.MvcApplication" Language="C#" %>
---
+++ Leaning Product/Leaning Product/Models/Model1DB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Leaning_Product.Models
+{
+    public class Model1DB
+    {
+        masterEntities db = new masterEntities();
+    }
+}
