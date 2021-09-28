317af588aa5896b89720cf9e57d9fe9815571c77
david77786 <davidsu77786@gmail.com>
2021-09-29 01:54:12 +0800

    新增Controller

---
+++ Leaning Product/Leaning Product/Data/Leaning_ProductContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Leaning_Product.Data
+{
+    public class Leaning_ProductContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Leaning_ProductContext() : base("name=Leaning_ProductContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Leaning_Product.Models.Guestbook> Guestbooks { get; set; }
+    }
+}
---
+++ Leaning Product/Leaning Product/Models/Guestbook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace Leaning_Product.Models
+{
+    public class Guestbook
+    {
+        [DisplayName("編號")]
+        public int id{get;set;}
+
+        [DisplayName("姓名")]
+        public string name { get; set; }
+
+        [DisplayName("內容")]
+        public string content { get; set; }
+    }
+}
