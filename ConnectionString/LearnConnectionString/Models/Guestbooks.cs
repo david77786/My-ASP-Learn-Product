2c65ea0625fb2ab170e4147d85055166da2a3130
david77786 <davidsu77786@gmail.com>
2021-09-30 21:55:14 +0800

    新增GuestbookDB(實作留言板)

--- ConnectionString/LearnConnectionString/Models/Guestbooks.cs
+++ ConnectionString/LearnConnectionString/Models/Guestbooks.cs
@@ -9,12 +9,26 @@ namespace LearnConnectionString.Models
     public class Guestbooks
     {
         [DisplayName("編號:")]
+        //編號
         public int id { get; set; }
 
         [DisplayName("姓名:")]
+        //名字
         public string Name { get; set; }
 
         [DisplayName("內容:")]
+        //留言內容
         public string Content { get; set; }
+
+        //新增時間
+        public DateTime CreateTime { get; set; }
+
+        //回覆內容
+        public string Reply { get; set; }
+
+        //回復時間
+        //DataTime ? 資料型態，允許DataTime 有Null產生
+        public DateTime? ReplyTime { get; set; }
+
     }
 }
---
+++ ConnectionString/LearnConnectionString/Services/GuestbooksDBService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace LearnConnectionString.Services
+{
+    public class GuestbooksDBService
+    {
+        private readonly static string cnstr = ConfigurationManager.ConnectionStrings["ASP.NET MVC"].ConnectionString;
+
+        private readonly SqlConnection conn = new SqlConnection(cnstr);
+    }
+}
